2e9b7ac0151a910c58748f8bd9e3654b9c484e71
GiridharSalana <GiridharSalana@gmail.com>
2020-07-27 09:11:44 +0530

    MyShellAnimation.sh Modified

---
+++ MyShellAnimation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+while [ : ]
+do
+    echo -ne '|\r'
+    sleep 0.3
+    echo -ne '/\r'
+    sleep 0.3
+    echo -ne '一\r'
+    sleep 0.3
+    echo -ne '\\ \r'
+    sleep 0.3
+    echo -ne '|\r'
+    sleep 0.3
+    echo -ne '$\r'
+    sleep 0.3
+done
