9f97d7b31ad50dce2305ebf0b97d47a26bebec21
GiridharSalana <GiridharSalana@gmail.com>
2020-08-11 19:26:52 +0530

    created Script for Creating Multiple files with single line code

---
+++ Multiple_Files&Dirs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#************************************************#
+#                                                #
+#      Author      :- Giridhar Salana            #
+#      Script Name :- Make_Files.sh              #
+#      Written on  :- 11-08-2019                 #
+#                                                #
+#************************************************#
+
+# Description : This Small and simple script used to create multiple files ending 
+
+
+touch Giri_{1...5} # This line of code will create Giri_1,Giri_2...Giri_5 files.
+mkdir Giri_{1...5} # This line of code will create Giri_1,Giri_2...Giri_5 Directories.
